35c2979619ad68db33578aa2a3236d4c4fa6596f
TheMechanist1 <44752216+TheMechanist1@users.noreply.github.com>
2019-07-18 12:33:27 -0500

    Added hitting affect and a block to manage the bots (#5)
    * Added Message Colors
    
    * Added The hitting affect when you swing your arm
    
    * Added a block to manage the bots

---
+++ src/main/java/net/neuralm/minecraftmod/blocks/BotManagerBlock.java
@@ -0,0 +1,9 @@
+package net.neuralm.minecraftmod.blocks;
+
+import net.minecraft.block.Block;
+
+public class BotManagerBlock extends Block {
+    public BotManagerBlock(Properties properties) {
+        super(properties);
+    }
+}
--- src/main/java/net/neuralm/minecraftmod/entities/renderer/BotEntityRenderer.java
+++ src/main/java/net/neuralm/minecraftmod/entities/renderer/BotEntityRenderer.java
@@ -1,20 +1,23 @@
 package net.neuralm.minecraftmod.entities.renderer;
 
-import static net.neuralm.minecraftmod.Neuralm.MODID;
-
-import javax.annotation.Nullable;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.entity.LivingRenderer;
-import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.client.renderer.entity.model.BipedModel;
 import net.minecraft.client.renderer.entity.model.PlayerModel;
 import net.minecraft.util.ResourceLocation;
 import net.neuralm.minecraftmod.entities.BotEntity;
 
-public class BotEntityRenderer extends LivingRenderer<BotEntity, PlayerModel<BotEntity>> {
+import javax.annotation.Nullable;
+
+import static net.neuralm.minecraftmod.Neuralm.MODID;
+
+public class BotEntityRenderer extends LivingRenderer<BotEntity, BipedModel<BotEntity>> {
 
     public BotEntityRenderer(EntityRendererManager rendererManager) {
         super(rendererManager, new PlayerModel<>(0, false), 0.5f);
+
     }
+
 
     @Nullable
     @Override
