f44fc804ed5ea2e65999a14a588ac9233834bae2
Mechanist <dminer625@gmail.com>
2019-07-18 08:42:27 -0500

    Added The hitting affect when you swing your arm

--- src/main/java/net/neuralm/minecraftmod/entities/BotEntity.java
+++ src/main/java/net/neuralm/minecraftmod/entities/BotEntity.java
@@ -2,9 +2,12 @@ package net.neuralm.minecraftmod.entities;
 
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Pose;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
 import net.minecraft.util.HandSide;
+import net.minecraft.util.NonNullList;
 import net.minecraft.world.ServerWorld;
 import net.minecraft.world.World;
 import net.minecraftforge.registries.ObjectHolder;
@@ -12,6 +15,10 @@ import net.neuralm.minecraftmod.Neuralm;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
 public class BotEntity extends LivingEntity {
+    public final NonNullList<ItemStack> mainInventory = NonNullList.withSize(36, ItemStack.EMPTY);
+    public final NonNullList<ItemStack> armorInventory = NonNullList.withSize(4, ItemStack.EMPTY);
+    public final NonNullList<ItemStack> offHandInventory = NonNullList.withSize(1, ItemStack.EMPTY);
+    public int currentItem;
 
     @ObjectHolder(Neuralm.MODID + ":bot")
     public static final EntityType<BotEntity> botEntityType = null;
@@ -24,25 +31,69 @@ public class BotEntity extends LivingEntity {
     }
 
     @Override
+    public void livingTick() {
+        super.livingTick();
+        this.updateArmSwingProgress();
+        this.updatePose();
+        this.updateEntityActionState();
+
+        this.setSneaking(true);
+
+        botSwingArm(Hand.MAIN_HAND);
+        botSwingArm(Hand.OFF_HAND);
+    }
+
+    public void botSwingArm(Hand hand) {
+        if (!isSwingInProgress) {
+            swingArm(hand);
+        }
+    }
+
+    public void updatePose() {
+        Pose pose;
+        if (this.isSneaking()) {
+            pose = Pose.SNEAKING;
+            this.setPose(pose);
+        }
+
+    }
+
+    @Override
     @NonNull
     public Iterable<ItemStack> getArmorInventoryList() {
-        return this.getFakePlayer().getArmorInventoryList();
+
+        return this.armorInventory;
     }
 
     @Override
     @NonNull
     public ItemStack getItemStackFromSlot(@NonNull EquipmentSlotType slotIn) {
-        return this.getFakePlayer().getItemStackFromSlot(slotIn);
+        if (slotIn == EquipmentSlotType.MAINHAND) {
+            return this.mainInventory.get(currentItem);
+        } else if (slotIn == EquipmentSlotType.OFFHAND) {
+            return this.offHandInventory.get(0);
+        } else {
+            return slotIn.getSlotType() == EquipmentSlotType.Group.ARMOR ? this.armorInventory.get(slotIn.getIndex()) : ItemStack.EMPTY;
+        }
     }
 
     @Override
     public void setItemStackToSlot(EquipmentSlotType slotIn, ItemStack stack) {
-        getFakePlayer().setItemStackToSlot(slotIn, stack);
+        if (slotIn == EquipmentSlotType.MAINHAND) {
+            this.playEquipSound(stack);
+            this.mainInventory.set(this.currentItem, stack);
+        } else if (slotIn == EquipmentSlotType.OFFHAND) {
+            this.playEquipSound(stack);
+            this.offHandInventory.set(0, stack);
+        } else if (slotIn.getSlotType() == EquipmentSlotType.Group.ARMOR) {
+            this.playEquipSound(stack);
+            this.armorInventory.set(slotIn.getIndex(), stack);
+        }
     }
 
     @Override
     public HandSide getPrimaryHand() {
-        return getFakePlayer().getPrimaryHand();
+        return HandSide.RIGHT;
     }
 
     private FakePlayer getFakePlayer() {
---
+++ src/main/java/net/neuralm/minecraftmod/networking/PacketHandler.java
@@ -0,0 +1,22 @@
+package net.neuralm.minecraftmod.networking;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.network.NetworkRegistry;
+import net.minecraftforge.fml.network.simple.SimpleChannel;
+import net.neuralm.minecraftmod.Neuralm;
+
+import java.util.Objects;
+
+public class PacketHandler {
+    public static SimpleChannel channel;
+    private static ResourceLocation networkName = new ResourceLocation(Neuralm.MODID, "net");
+
+    public static void registerChannel() {
+        channel = NetworkRegistry.ChannelBuilder.named(networkName).
+                //Update version number when updated
+                        clientAcceptedVersions(s -> Objects.equals(s, "1")).
+                        serverAcceptedVersions(s -> Objects.equals(s, "1")).
+                        networkProtocolVersion(() -> "1").
+                        simpleChannel();
+    }
+}
